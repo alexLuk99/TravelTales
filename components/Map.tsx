f91dba8fb9bca944be1edc2038a0ff17c13828f3
alexLuk99 <87333637+alexLuk99@users.noreply.github.com>
2024-11-20 20:17:05 +0100

    outsource code fragments

--- components/Map.tsx
+++ components/Map.tsx
@@ -2,27 +2,12 @@ import { Text, View } from "react-native";
 import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
 import * as Location from 'expo-location';
 import { useEffect, useState } from "react";
+import useUserLocation from "@/hooks/useUserLocation";
 
 Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
 
 export default function Map() {
-    const [location, setLocation] = useState<Location.LocationObject | null>(null);
-    const [errorMsg, setErrorMsg] = useState<string | null>(null);
-
-    useEffect(() => {
-        async function getCurrentLocation() {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                setErrorMsg('Permission to access location was denied');
-                return;
-            }
-
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
-        }
-
-        getCurrentLocation();
-    }, []);
+    const { location, errorMsg } = useUserLocation();
 
     let text = 'Waiting...';
     if (errorMsg) {
@@ -33,7 +18,10 @@ export default function Map() {
 
     return (
         <MapView style={{flex: 1}} styleURL="mapbox://styles/mapbox/standard" projection="globe">
-            <Camera followZoomLevel={1} followUserLocation />
+            <Camera 
+                followZoomLevel={1} 
+                followUserLocation 
+            />
             <LocationPuck pulsing={{ isEnabled:true }}/>
         </MapView>  
     );
---
+++ hooks/useUserLocation.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
+
+export default function useUserLocation() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  return { location, errorMsg };
+}
