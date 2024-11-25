4b73c5e60a80d83c7e4e84b546e5953f4184a3fb
alexLuk99 <87333637+alexLuk99@users.noreply.github.com>
2024-11-25 21:08:40 +0100

    add layer ove every country with either a all or Us world view

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,5 +1,5 @@
 import { Text, View } from "react-native";
-import Mapbox, { Camera, LocationPuck, MapView } from '@rnmapbox/maps';
+import Mapbox, { Camera, FillLayer, LocationPuck, MapView, VectorSource } from '@rnmapbox/maps';
 import * as Location from 'expo-location';
 import { useEffect, useState } from "react";
 import useUserLocation from "@/hooks/useUserLocation";
@@ -8,6 +8,10 @@ Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_KEY || '');
 
 export default function Map() {
     const { location, errorMsg } = useUserLocation();
+    const fillLayerStyle = {
+        fillColor: '#3bb2d0', // Globale Füllfarbe
+        fillOpacity: 0.5,    // Transparenz
+    };
 
     let text = 'Waiting...';
     if (errorMsg) {
@@ -18,6 +22,34 @@ export default function Map() {
 
     return (
         <MapView style={{flex: 1}} styleURL="mapbox://styles/mapbox/standard" projection="globe">
+            <VectorSource id="global-layer-source" url="mapbox://mapbox.country-boundaries-v1">
+                <FillLayer 
+                    id="country-layer" 
+                    sourceLayerID="country_boundaries" 
+                    style={fillLayerStyle} 
+                    filter={[
+                        "all",
+                        [
+                          "==",
+                          ["get", "disputed"],
+                          "false"
+                        ],
+                        [
+                          "any",
+                          [
+                            "==",
+                            "all",
+                            ["get", "worldview"]
+                          ],
+                          [
+                            "in",
+                            "US",
+                            ["get", "worldview"]
+                          ]
+                        ]
+                      ]}
+                />
+            </VectorSource>
             <Camera 
                 followZoomLevel={1} 
                 followUserLocation 
